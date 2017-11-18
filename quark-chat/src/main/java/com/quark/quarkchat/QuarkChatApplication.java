ae09649645dc0e839fa7bd1167d058d94eccfa55
Livours <877236841@qq.com>
2017-11-18 14:20:20 +0800

    chat-test

---
+++ quark-chat/src/main/java/com/quark/quarkchat/QuarkChatApplication.java
@@ -0,0 +1,12 @@
+package com.quark.quarkchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuarkChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuarkChatApplication.class, args);
+	}
+}
